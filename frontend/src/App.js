8c5b84283013aa118d2baccfe0c2e4f96fb86a8c
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-05-26 18:53:35 +0200

    Update component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "./components/Layout";
+import Layout from "./pages/Layout";
 
 const routerSettings = createBrowserRouter([
   { path: "/", element: <Layout /> },
