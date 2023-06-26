a88785d16b1dabdf3ab03ef33afb99082e53a0f5
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-06-26 21:05:58 +0200

    Add navlinks

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 import classes from "./Navbar.module.scss";
 
 const Navbar = () => {
@@ -5,9 +7,9 @@ const Navbar = () => {
     <>
     <header className={classes.header}>
       <nav className={classes.navbar}>
-        <h1>PizzApp</h1>
+        <h1><NavLink to="/">PizzApp</NavLink></h1>
         <ul>
-          <li>Pizzas</li>
+          <li><NavLink to="/pizzas">Pizzas</NavLink></li>
           <li>My Order</li>
         </ul>
       </nav>
